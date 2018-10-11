8cea05741ea0339274de6ad6b11a9cf03a4fcc9a
undeadevs <toxicfre4king@gmail.com>
2018-10-11 19:06:51 +0800

    Add animequote command

---
+++ commands/random/animequote.js
@@ -0,0 +1,36 @@
+var animeQuotes = require('animequotes');
+
+const commando = require('discord.js-commando');
+const {RichEmbed} = require('discord.js');
+
+class aQuoteCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'animequote',
+            aliases: ['anime-quote'],
+            group: 'random',
+            memberName: 'animequote',
+            description: 'Shows random anime quotes.'
+        });
+    }
+
+    async run(message, args)
+    {
+        var aq = animeQuotes.randomQuote();
+
+    var qs = aq.quote;
+    var qc = aq.name;
+    var qa = aq.anime;
+        const qEmbed = new RichEmbed()
+            .setDescription(`
+*\"${aq.quote}\"*
+**-${aq.name} [${aq.anime}]**
+`);
+        message.channel.sendMessage(qEmbed);
+    }
+
+}
+
+module.exports = aQuoteCommand;
