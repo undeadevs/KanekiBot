2fc94e814b49fbe4b5d514b5b5b6d27450e9f3e7
undeadevs <toxicfre4king@gmail.com>
2019-06-24 22:44:58 +0800

    UP

--- commands/guild/filter.js
+++ commands/guild/filter.js
@@ -1,11 +1,9 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
 
-class filterCommand extends commando.Command
-{
-    constructor(client)
-    {
-        super(client,{
+class filterCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
             name: 'filter',
             group: 'guild',
             memberName: 'filter',
@@ -15,20 +13,23 @@ class filterCommand extends commando.Command
             args: [
                 {
                     key: 'toggle',
-                    prompt:'Filter Bad Words on/off?',
+                    prompt: 'Filter Bad Words on/off?',
                     type: 'string'
                 }
             ]
         });
     }
 
-    async run(message, { toggle })
-    {
+    async run(message, { toggle }) {
         console.log(global.test);
-        if(toggle == `on`){ return global.test = true;}else
-        if(toggle == `off`){ return global.test = false;}else{
-            return message.say(`You can only choose either on or off.`);
-        }
+        if (toggle == `on`) {
+            return global.test = true;
+        } else
+            if (toggle == `off`) {
+                return global.test = false;
+            } else {
+                return message.say(`You can only choose either on or off.`);
+            }
     }
 
 }
