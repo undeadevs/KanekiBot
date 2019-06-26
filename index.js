726ef8454f435066082210d5b056da910d627729
undeadevs <toxicfre4king@gmail.com>
2019-06-26 11:22:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ bot.on('ready', function () {
 
     bot.user.setActivity('Unravel', {type: 'LISTENING'});*/
     console.log(`[Bot is online | Node: ${process.version} | Discord.js-Commando: v${Commando.version}]\nConnected as: ${bot.user.username} (ID: ${bot.user.id})\nGuilds Connected: ${bot.guilds.size}`);
-        while (true) {
             bot.user.setActivity(`${bot.guilds.size} Servers | ${process.env.prefix}help`, { type: 'WATCHING' });
-        }
 });
 
 global.queue = {};
