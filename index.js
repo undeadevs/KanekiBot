7332d3387bc8b0f190db856e433fd91804a0d87f
undeadevs <toxicfre4king@gmail.com>
2019-06-26 09:54:59 +0800

    UP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Commando = require('discord.js-commando');
 const fs = require('fs');
 const dotenv = require('dotenv').config();
-const bot = new Commando.Client({owner: [process.env.ownerID, process.env.adminID0, process.env.adminID1], commandPrefix: process.env.prefix, unknownCommandResponse: false});
+const bot = new Commando.Client({ owner: [process.env.ownerID, process.env.adminID0, process.env.adminID1], commandPrefix: process.env.prefix, unknownCommandResponse: false });
 
 const ytdl = require('ytdl-core');
 const yt = require('simple-youtube-api');
@@ -20,15 +20,19 @@ bot.registry.registerGroup('analyze', 'Analyze');
 bot.registry.registerGroup('search', 'Search');
 bot.registry.registerGroup('ppedit', 'Avatar Edit');
 bot.registry.registerDefaultGroups();
-bot.registry.registerDefaultCommands({ help:false, prefix:false, eval:false, ping:false});
+bot.registry.registerDefaultCommands({ help: false, prefix: false, eval: false, ping: false });
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
-bot.on('ready',function(){
+bot.on('ready', function () {
     /*console.log("Login Success");
 
     bot.user.setActivity('Unravel', {type: 'LISTENING'});*/
     console.log(`[Bot is online | Node: ${process.version} | Discord.js-Commando: v${Commando.version}]\nConnected as: ${bot.user.username} (ID: ${bot.user.id})\nGuilds Connected: ${bot.guilds.size}`);
-    bot.user.setActivity(`${bot.guilds.size} Servers | ${process.env.prefix}help`, {type: 'WATCHING'});
+    setInterval(function () {
+        while (true) {
+            bot.user.setActivity(`${bot.guilds.size} Servers | ${process.env.prefix}help`, { type: 'WATCHING' });
+        }
+    }, 30000);
 });
 
 global.queue = {};
@@ -36,47 +40,47 @@ global.queue = {};
 bot.msgs = require("./msgs.json");
 
 try {
-bot.on('guildMemberAdd', function(member){
-    //DYas = member.guilds.find("name", "DYas");
-    //tst = member.guilds.find("name", "tst");
+    bot.on('guildMemberAdd', function (member) {
+        //DYas = member.guilds.find("name", "DYas");
+        //tst = member.guilds.find("name", "tst");
 
-    //this is for my server
-    if(bot.isOwner(member)){
-        let or = member.guild.roles.find("name", "owner");
-        let gc = member.guild.channels.find("name", "lobby");
-        if(!gc) return;
-        if(!or){
-            return gc.send(['Welcome to the Server ' + member, rules]);
+        //this is for my server
+        if (bot.isOwner(member)) {
+            let or = member.guild.roles.find("name", "owner");
+            let gc = member.guild.channels.find("name", "lobby");
+            if (!gc) return;
+            if (!or) {
+                return gc.send(['Welcome to the Server ' + member, rules]);
+            }
+            member.addRole(or);
+            gc.send('Welcome to the Server my owner!!!');
+        } else if (!bot.isOwner(member)) {
+            let mr = member.guild.roles.find("name", "trash");
+            let gc = member.guild.channels.find("name", "lobby");
+            if (!gc) return;
+            if (!mr) {
+                return gc.send(['Welcome to the Server ' + member]);
+            }
+            member.addRole(mr);
+            let rules = "RULES : \n-do not insult anyone.";
+            gc.send(['Welcome to the Server ' + member, rules]);
+
         }
-        member.addRole(or);
-        gc.send('Welcome to the Server my owner!!!');
-    }else if(!bot.isOwner(member)){
-        let mr = member.guild.roles.find("name", "trash");
-        let gc = member.guild.channels.find("name", "lobby");
-        if(!gc) return;
-        if(!mr){
-            return gc.send(['Welcome to the Server ' + member]);
-        }
-        member.addRole(mr);
-        let rules = "RULES : \n-do not insult anyone.";
-        gc.send(['Welcome to the Server ' + member, rules]);
+        //end
 
-    }
-    //end
+        //member.send('Welcome to the Server ' + member);
 
-    //member.send('Welcome to the Server ' + member);
-
-});
+    });
 }
-catch(error){}
+catch (error) { }
 
 global.test = false;
 
-bot.on('message', function(message){
+bot.on('message', function (message) {
     mention = message.mentions.users.first();
     msg = message.content.toLowerCase();
-    
-    if(message.author.bot) return;
+
+    if (message.author.bot) return;
 
 });
 
